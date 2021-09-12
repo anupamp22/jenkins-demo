126a184fe155457e14079413808752414230b4cb
anupam.p22 <anupam.p22@gmail.com>
2021-09-12 15:36:23 -0700

    adding theme-park-job-pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/anupamp22/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
